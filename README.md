d4c16cddcb440e081ce508d07c809f39b514955c
myutzy <matt@ctmstudios.net>
2022-06-01 21:50:53 -0400

    Add hardhat project and React + Next app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# nft-whitelist
+# NFT Whitelist
 Ethereum NFT whitelist dapp created as part of studying Solidity utilizing Node, Hardhat, Vercel and the Goerli testnet.
+
+Contract deployed to Ethereum Goerli testnet at address `0xF22f5590793Ff160952e7cEbDf799D6FeDd7714d`
+
+### Credit
+- Completed as part of Web3 learning within LearnWeb3 Dao at https://www.learnweb3.io/
---
+++ app/constants/index.js
@@ -0,0 +1,66 @@
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_maxWhitelistedAddreseses",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "addAddressToWhitelist",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "maxWhitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "numAddressesWhitelisted",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "whitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+export const WHITELIST_CONTRACT_ADDRESS = "0xF22f5590793Ff160952e7cEbDf799D6FeDd7714d";
